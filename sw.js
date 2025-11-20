2d752a37d87d208d1df1540ca0c6f1c7f763cb23
Rajeh Alrabie <rajehalrabie@gmail.com>
2025-11-20 11:50:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "thrivve-tracker-v4.2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./report.html",
+  "./manifest.webmanifest",
+  "./icon-512.png",
+  "./icon-196.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return (
+        res ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+      );
+    })
+  );
+});
